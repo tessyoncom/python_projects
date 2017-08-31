4a59fc2f765e78d96cb7f15d9772fd8a9b3cb589
tessy <tessyoncom@gmail.com>
2017-08-31 15:29:49 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+something = "Bi a ba pe laye"
+print(something)
+print ("wow, did this work???")
+
+j = [3,7,9,"bola","yes",78]
+
+for a in j:
+    print(a)
+    print("this is the end of the code")
